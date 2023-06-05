529a212fd8204d032f7107d2e762b30b8f5474f9
dresdner353 <dresdner353@gmail.com>
2023-06-05 09:59:19 +0100

    updated examples

--- sample_data/gen_reports.sh
+++ sample_data/gen_reports.sh
@@ -46,3 +46,59 @@ python3 ${GEN_REPORT_SCRIPT} \
     --tariff_interval 08-23:Day 23-08:Night 7-7:08-23:Free \
     --standing_rate 0.0346 \
     --fit_rate 0.21
+
+python3 ${GEN_REPORT_SCRIPT} \
+    --idir ${HDF_DATA} \
+    --file bge_report_24h.xlsx \
+    --tariff_rate 24h:0.4819 \
+    --tariff_interval 00-00:24h \
+    --standing_rate 0.0321 \
+    --fit_rate 0.185
+
+python3 ${GEN_REPORT_SCRIPT} \
+    --idir ${HDF_DATA} \
+    --file bge_report_standard_smart.xlsx \
+    --tariff_rate Day:0.5149 Night:0.3802 Peak:0.6270 \
+    --tariff_interval 08-23:Day 23-08:Night 17-19:Peak \
+    --standing_rate 0.0321 \
+    --fit_rate 0.185
+
+python3 ${GEN_REPORT_SCRIPT} \
+    --idir ${HDF_DATA} \
+    --file bge_report_discount_weekend.xlsx \
+    --tariff_rate Day:0.5071 Night:0.4534 Peak:0.6185 Weekend:0.4534\
+    --tariff_interval 08-23:Day 23-08:Night 17-19:Peak 6-7:00-00:Weekend \
+    --standing_rate 0.0321 \
+    --fit_rate 0.185
+
+python3 ${GEN_REPORT_SCRIPT} \
+    --idir ${HDF_DATA} \
+    --file bge_report_tou.xlsx \
+    --tariff_rate Day:0.5196 Night:0.3743 Peak:0.6224 \
+    --tariff_interval 08-23:Day 23-08:Night 17-19:Peak \
+    --standing_rate 0.0321 \
+    --fit_rate 0.185
+
+python3 ${GEN_REPORT_SCRIPT} \
+    --idir ${HDF_DATA} \
+    --file bge_report_freetime_sat.xlsx \
+    --tariff_rate Day:0.5094 Night:0.3743 Peak:0.6223  Free:0 \
+    --tariff_interval 08-23:Day 23-08:Night 17-19:Peak 6-6:09-17:Free \
+    --standing_rate 0.0321 \
+    --fit_rate 0.185
+
+python3 ${GEN_REPORT_SCRIPT} \
+    --idir ${HDF_DATA} \
+    --file bge_report_freetime_sun.xlsx \
+    --tariff_rate Day:0.5094 Night:0.3743 Peak:0.6223  Free:0 \
+    --tariff_interval 08-23:Day 23-08:Night 17-19:Peak 7-7:09-17:Free \
+    --standing_rate 0.0321 \
+    --fit_rate 0.185
+
+python3 ${GEN_REPORT_SCRIPT} \
+    --idir ${HDF_DATA} \
+    --file bge_report_ev.xlsx \
+    --tariff_rate Day:0.4803 Night:0.3622 Peak:0.6697 EV:0.1224 \
+    --tariff_interval 08-23:Day 23-08:Night 02-05:EV 1-5:17-19:Peak \
+    --standing_rate 0.0478 \
+    --fit_rate 0.185
