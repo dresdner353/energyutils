c0e5ad320c849ad057a745bacadc9e70573ec5ce
dresdner353 <dresdner353@gmail.com>
2023-06-05 20:58:18 +0100

    moved tariff ahead of other fields as its a pivot field for the final tariff graphs

--- gen_report.py
+++ gen_report.py
@@ -682,12 +682,6 @@ field_dict = {
             'format' : 'integer',
             },
 
-        'standing_cost' : {
-            'title' : 'Standing Cost',
-            'width' : 12,
-            'header_format' : 'header',
-            'format' : 'float',
-            },
         'tariff_name' : {
             'title' : 'Import Tariff',
             'width' : 12,
@@ -696,6 +690,13 @@ field_dict = {
             },
         'tariff_rate' : {
             'title' : 'Import Rate',
+            'width' : 12,
+            'header_format' : 'header',
+            'format' : 'float',
+            },
+
+        'standing_cost' : {
+            'title' : 'Standing Cost',
             'width' : 12,
             'header_format' : 'header',
             'format' : 'float',
