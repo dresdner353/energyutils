af05feb1e18c59fc76dad9b05d55142292c4a8ba
dresdner353 <dresdner353@gmail.com>
2023-06-05 19:32:00 +0100

    tweeked to generate on one directory

--- sample_data/README.md
+++ sample_data/README.md
@@ -1,8 +1,6 @@
 # Sample Data
 
-This directory contains sample data (HDF_example.csv) from an ESB smart meter. In addition there is a selection of generated reports for various providers and a script that can generate these reports. 
-
-Each report applies pricing details from a particular provider plan. Note that the rates used may be out of date and should be checked.
+This directory contains sample data (HDF_example.csv) from an ESB smart meter. In addition there is a script that can generate reports based on pricing details from provider plans. Note that the rates used may be out of date and should be checked.
 
 You could copy your ESB HDF file over the HDF_example.csv and then run gen_reports.sh to regenerate all reports and obtain a price model for you usage against each plan.
 
--- sample_data/gen_reports.sh
+++ sample_data/gen_reports.sh
@@ -1,4 +1,4 @@
-HDF_DATA=/tmp/hdf_data
+HDF_DATA=hdf_data
 GEN_REPORT_SCRIPT=../gen_report.py
 
 mkdir -p ${HDF_DATA}
@@ -9,7 +9,7 @@ python3 ../esb_hdf_reader.py --file HDF_example.csv  --odir ${HDF_DATA}
 
 python3 ${GEN_REPORT_SCRIPT} \
     --idir ${HDF_DATA} \
-    --file EI/ei_report_24h.xlsx \
+    --file ei_report_24h.xlsx \
     --tariff_rate 24h:0.3959 \
     --tariff_interval 00-00:24h \
     --standing_rate 0.0346 \
@@ -17,7 +17,7 @@ python3 ${GEN_REPORT_SCRIPT} \
 
 python3 ${GEN_REPORT_SCRIPT} \
     --idir ${HDF_DATA} \
-    --file EI/ei_report_electricplus.xlsx \
+    --file ei_report_electricplus.xlsx \
     --tariff_rate Day:0.4451 Night:0.2339 Peak:0.4746 \
     --tariff_interval 08-23:Day 23-08:Night 17-19:Peak \
     --standing_rate 0.0346 \
@@ -25,7 +25,7 @@ python3 ${GEN_REPORT_SCRIPT} \
 
 python3 ${GEN_REPORT_SCRIPT} \
     --idir ${HDF_DATA} \
-    --file EI/ei_report_dualplus.xlsx \
+    --file ei_report_dualplus.xlsx \
     --tariff_rate Day:0.4310 Night:0.2265 Peak:0.4596 \
     --tariff_interval 08-23:Day 23-08:Night 17-19:Peak \
     --standing_rate 0.0346 \
@@ -33,7 +33,7 @@ python3 ${GEN_REPORT_SCRIPT} \
 
 python3 ${GEN_REPORT_SCRIPT} \
     --idir ${HDF_DATA} \
-    --file EI/ei_report_dualboost.xlsx \
+    --file ei_report_dualboost.xlsx \
     --tariff_rate Day:0.4320 Night:0.2086 Boost:0.1225 \
     --tariff_interval 08-23:Day 23-08:Night 02-04:Boost \
     --standing_rate 0.0453 \
@@ -41,7 +41,7 @@ python3 ${GEN_REPORT_SCRIPT} \
 
 python3 ${GEN_REPORT_SCRIPT} \
     --idir ${HDF_DATA} \
-    --file EI/ei_report_sat_free.xlsx \
+    --file ei_report_sat_free.xlsx \
     --tariff_rate Day:0.4346 Night:0.4346 Free:0 \
     --tariff_interval 08-23:Day 23-08:Night 6-6:08-23:Free \
     --standing_rate 0.0346 \
@@ -49,7 +49,7 @@ python3 ${GEN_REPORT_SCRIPT} \
     
 python3 ${GEN_REPORT_SCRIPT} \
     --idir ${HDF_DATA} \
-    --file EI/ei_report_sun_free.xlsx \
+    --file ei_report_sun_free.xlsx \
     --tariff_rate Day:0.4346 Night:0.4346 Free:0 \
     --tariff_interval 08-23:Day 23-08:Night 7-7:08-23:Free \
     --standing_rate 0.0346 \
@@ -57,7 +57,7 @@ python3 ${GEN_REPORT_SCRIPT} \
 
 python3 ${GEN_REPORT_SCRIPT} \
     --idir ${HDF_DATA} \
-    --file BGE/bge_report_24h.xlsx \
+    --file bge_report_24h.xlsx \
     --tariff_rate 24h:0.4819 \
     --tariff_interval 00-00:24h \
     --standing_rate 0.0321 \
@@ -65,7 +65,7 @@ python3 ${GEN_REPORT_SCRIPT} \
 
 python3 ${GEN_REPORT_SCRIPT} \
     --idir ${HDF_DATA} \
-    --file BGE/bge_report_standard_smart.xlsx \
+    --file bge_report_standard_smart.xlsx \
     --tariff_rate Day:0.5149 Night:0.3802 Peak:0.6270 \
     --tariff_interval 08-23:Day 23-08:Night 17-19:Peak \
     --standing_rate 0.0321 \
@@ -73,7 +73,7 @@ python3 ${GEN_REPORT_SCRIPT} \
 
 python3 ${GEN_REPORT_SCRIPT} \
     --idir ${HDF_DATA} \
-    --file BGE/bge_report_discount_weekend.xlsx \
+    --file bge_report_discount_weekend.xlsx \
     --tariff_rate Day:0.5071 Night:0.4534 Peak:0.6185 Weekend:0.4534\
     --tariff_interval 08-23:Day 23-08:Night 17-19:Peak 6-7:00-00:Weekend \
     --standing_rate 0.0321 \
@@ -81,7 +81,7 @@ python3 ${GEN_REPORT_SCRIPT} \
 
 python3 ${GEN_REPORT_SCRIPT} \
     --idir ${HDF_DATA} \
-    --file BGE/bge_report_tou.xlsx \
+    --file bge_report_tou.xlsx \
     --tariff_rate Day:0.5196 Night:0.3743 Peak:0.6224 \
     --tariff_interval 08-23:Day 23-08:Night 17-19:Peak \
     --standing_rate 0.0321 \
@@ -89,7 +89,7 @@ python3 ${GEN_REPORT_SCRIPT} \
 
 python3 ${GEN_REPORT_SCRIPT} \
     --idir ${HDF_DATA} \
-    --file BGE/bge_report_freetime_sat.xlsx \
+    --file bge_report_freetime_sat.xlsx \
     --tariff_rate Day:0.5094 Night:0.3743 Peak:0.6223  Free:0 \
     --tariff_interval 08-23:Day 23-08:Night 17-19:Peak 6-6:09-17:Free \
     --standing_rate 0.0321 \
@@ -97,7 +97,7 @@ python3 ${GEN_REPORT_SCRIPT} \
 
 python3 ${GEN_REPORT_SCRIPT} \
     --idir ${HDF_DATA} \
-    --file BGE/bge_report_freetime_sun.xlsx \
+    --file bge_report_freetime_sun.xlsx \
     --tariff_rate Day:0.5094 Night:0.3743 Peak:0.6223  Free:0 \
     --tariff_interval 08-23:Day 23-08:Night 17-19:Peak 7-7:09-17:Free \
     --standing_rate 0.0321 \
@@ -105,7 +105,7 @@ python3 ${GEN_REPORT_SCRIPT} \
 
 python3 ${GEN_REPORT_SCRIPT} \
     --idir ${HDF_DATA} \
-    --file BGE/bge_report_ev.xlsx \
+    --file bge_report_ev.xlsx \
     --tariff_rate Day:0.4803 Night:0.3622 Peak:0.6697 EV:0.1224 \
     --tariff_interval 08-23:Day 23-08:Night 02-05:EV 1-5:17-19:Peak \
     --standing_rate 0.0478 \
@@ -113,7 +113,7 @@ python3 ${GEN_REPORT_SCRIPT} \
 
 python3 ${GEN_REPORT_SCRIPT} \
     --idir ${HDF_DATA} \
-    --file SSE/sse_report_boost.xlsx \
+    --file sse_report_boost.xlsx \
     --tariff_rate Day:0.4830 Night:0.2735 Peak:0.6492 Boost:0.1055 \
     --tariff_interval 08-23:Day 23-08:Night 02-05:Boost 17-19:Peak \
     --standing_rate 0.0349 \
@@ -121,7 +121,7 @@ python3 ${GEN_REPORT_SCRIPT} \
 
 python3 ${GEN_REPORT_SCRIPT} \
     --idir ${HDF_DATA} \
-    --file ENERGIA/energia_report_smart.xlsx \
+    --file energia_report_smart.xlsx \
     --tariff_rate Day:0.4576 Night:0.2450 Peak:0.4794 \
     --tariff_interval 08-23:Day 23-08:Night 17-19:Peak \
     --standing_rate 0.0270 \
@@ -129,7 +129,7 @@ python3 ${GEN_REPORT_SCRIPT} \
 
 python3 ${GEN_REPORT_SCRIPT} \
     --idir ${HDF_DATA} \
-    --file ENERGIA/energia_report_ev.xlsx \
+    --file energia_report_ev.xlsx \
     --tariff_rate Day:0.5262 Night:0.2817 Peak:0.5753 EV:0.1264 \
     --tariff_interval 08-23:Day 23-08:Night 02-06:EV 17-19:Peak \
     --standing_rate 0.0270 \
@@ -137,7 +137,7 @@ python3 ${GEN_REPORT_SCRIPT} \
 
 python3 ${GEN_REPORT_SCRIPT} \
     --idir ${HDF_DATA} \
-    --file FLOGAS/flogas_report_smart.xlsx \
+    --file flogas_report_smart.xlsx \
     --tariff_rate Day:0.3680 Night:0.2868 Peak:0.4352 \
     --tariff_interval 08-23:Day 23-08:Night 17-19:Peak \
     --standing_rate 0.0387 \
@@ -145,7 +145,7 @@ python3 ${GEN_REPORT_SCRIPT} \
 
 python3 ${GEN_REPORT_SCRIPT} \
     --idir ${HDF_DATA} \
-    --file PINERGY/pinergy_report_work_from_home.xlsx \
+    --file pinergy_report_work_from_home.xlsx \
     --tariff_rate Work:0.3263 Peak:0.4662 \
     --tariff_interval 09-17:Work 17-09:Peak \
     --standing_rate 0.0324 \
@@ -153,7 +153,7 @@ python3 ${GEN_REPORT_SCRIPT} \
 
 python3 ${GEN_REPORT_SCRIPT} \
     --idir ${HDF_DATA} \
-    --file PINERGY/pinergy_report_family_time.xlsx \
+    --file pinergy_report_family_time.xlsx \
     --tariff_rate Evening:0.2797 Peak:0.4662 \
     --tariff_interval 19-00:Evening 00-19:Peak \
     --standing_rate 0.0324 \
@@ -161,7 +161,7 @@ python3 ${GEN_REPORT_SCRIPT} \
 
 python3 ${GEN_REPORT_SCRIPT} \
     --idir ${HDF_DATA} \
-    --file PINERGY/pinergy_report_ev.xlsx \
+    --file pinergy_report_ev.xlsx \
     --tariff_rate EV:0.0692 Peak:0.4662 \
     --tariff_interval 02-05:EV 05-02:Peak \
     --standing_rate 0.0324 \
@@ -169,8 +169,9 @@ python3 ${GEN_REPORT_SCRIPT} \
 
 python3 ${GEN_REPORT_SCRIPT} \
     --idir ${HDF_DATA} \
-    --file PINERGY/pinergy_report_standard_smart.xlsx \
+    --file pinergy_report_standard_smart.xlsx \
     --tariff_rate Day:0.4646 Night:0.3546 Peak:0.4992 \
     --tariff_interval 08-23:Day 23-08:Night 17-19:Peak \
     --standing_rate 0.0324 \
     --fit_rate 0.21
+
