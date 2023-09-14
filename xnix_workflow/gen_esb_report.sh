85b5a4d4a0e88b9f18d254b77cd98d4dcdc66d0c
dresdner353 <dresdner353@gmail.com>
2023-09-14 12:18:08 +0100

    first stab at a MacOS/Linux offering for the workflow

---
+++ xnix_workflow/gen_esb_report.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+
+HDF_FILE='esb_hdf.csv'
+HDF_DATA='hdf_data'
+ESB_SCRIPT=../esb_hdf_reader.py
+GEN_REPORT_SCRIPT=../gen_report.py
+
+mkdir -p "${HDF_DATA}"
+rm -f "${HDF_DATA}"/*
+
+# Read ESB HDF file and export JSONL files
+python3 ${ESB_SCRIPT} \
+    --file "${HDF_FILE}"  \
+    --odir "${HDF_DATA}"
+
+# Example EV report
+python3 ${GEN_REPORT_SCRIPT} \
+    --idir "${HDF_DATA}" \
+    --file esb_report.xlsx \
+    --tariff_rate Day:0.4320 Night:0.2086 Boost:0.1225 \
+    --tariff_interval 08-23:Day 23-08:Night 02-04:Boost \
+    --standing_rate 0.0453 \
+    --fit_rate 0.21 
+
