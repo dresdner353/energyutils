e6e7f206e43805fd19d326ddac142fb926043db3
dresdner353 <dresdner353@gmail.com>
2023-10-03 10:47:16 +0100

    added the reports option

--- xnix_workflow/gen_esb_report.sh
+++ xnix_workflow/gen_esb_report.sh
@@ -5,6 +5,7 @@ HDF_FILE='esb_hdf.csv'
 HDF_DATA='hdf_data'
 ESB_SCRIPT=../esb_hdf_reader.py
 GEN_REPORT_SCRIPT=../gen_report.py
+REPORTS=day week month year hour tariff 24h weekday
 
 mkdir -p "${HDF_DATA}"
 rm -f "${HDF_DATA}"/*
@@ -18,6 +19,7 @@ python3 ${ESB_SCRIPT} \
 python3 ${GEN_REPORT_SCRIPT} \
     --idir "${HDF_DATA}" \
     --file esb_report.xlsx \
+    --reports ${REPORTS} \
     --tariff_rate Day:0.4320 Night:0.2086 Boost:0.1225 \
     --tariff_interval 08-23:Day 23-08:Night 02-04:Boost \
     --standing_rate 0.0453 \
