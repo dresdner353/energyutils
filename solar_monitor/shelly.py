88f52e4b726f931f142d2ed8e5d102349e6a1e0a
dresdner353 <dresdner353@gmail.com>
2024-01-26 08:26:44 +0000

    restored the 5-sec refresh which the Shelly EM/Pro is more than capable of supporting

--- solar_monitor/shelly.py
+++ solar_monitor/shelly.py
@@ -517,5 +517,5 @@ def get_data(config):
                 ) 
             )
 
-    # return data and fixed sleep of 10 seconds for refresh
-    return gv_shelly_dict, 10
+    # return data and fixed sleep of 5 seconds for refresh
+    return gv_shelly_dict, 5
