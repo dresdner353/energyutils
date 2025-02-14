14746b0bd5f694604f48e99182c0313b6788b464
dresdner353 <dresdner353@gmail.com>
2025-02-14 09:42:33 +0000

    dedicated setup and run batch files for Windows

---
+++ solar_monitor/windows_run.bat
@@ -0,0 +1,6 @@
+@echo off
+SET PYTHON=python3.exe
+SET SOLAR_MON_WEB_SVR=solar_monitor.py
+
+%PYTHON% %SOLAR_MON_WEB_SVR%
+
---
+++ solar_monitor/windows_setup.bat
@@ -0,0 +1,3 @@
+set PYTHON=python3.exe
+%PYTHON% -m pip install tzdata requests xlsxwriter python-dateutil cherrypy
+pause
