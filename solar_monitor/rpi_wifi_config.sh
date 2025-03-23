4cbecebf5d45b6edcc04a04b5b27257c83c9c6bf
dresdner353 <dresdner353@gmail.com>
2025-03-23 19:17:16 +0000

    tweaks

--- solar_monitor/rpi_wifi_config.sh
+++ solar_monitor/rpi_wifi_config.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+NM_CONN_DIR="/etc/NetworkManager/system-connections"
+NM_CONN_FILE="${NM_CONN_DIR}/solarmon.nmconnection"
+
 WIFI_CONFIG_FILE=`find /media -name wifi.txt | tail -1`
 
 if test -f "${WIFI_CONFIG_FILE}"; then
@@ -8,16 +11,15 @@ if test -f "${WIFI_CONFIG_FILE}"; then
     DIR=`dirname ${WIFI_CONFIG_FILE}`
 
     . "$WIFI_CONFIG_FILE"
-    rm -f /etc/NetworkManager/system-connections/*
+    rm -f ${NM_CONN_DIR}/*
     cat /home/pi/energyutils/solar_monitor/rpi.nmconnection.tmpl | \
-        sed -e "s/__SSID__/${SSID}/g" -e "s/__PASSWORD__/${PASSWORD}/g" > \
-        /etc/NetworkManager/system-connections/solarmon.nmconnection
-    chmod go-rw /etc/NetworkManager/system-connections/solarmon.nmconnection
+        sed -e "s/__SSID__/${SSID}/g" -e "s/__PASSWORD__/${PASSWORD}/g" > ${NM_CONN_FILE}
+    chmod go-rw ${NM_CONN_FILE}
     systemctl restart NetworkManager
     echo "Configured WiFi for ${SSID}"
 
     # reboot
-    reboot
+    /usr/sbin/reboot
 fi
 
 
