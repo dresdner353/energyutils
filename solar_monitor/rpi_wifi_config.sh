8a595344401cdc9085ef0ebb7bc8cc05a478a133
dresdner353 <dresdner353@gmail.com>
2025-03-23 18:58:04 +0000

    tweaks

--- solar_monitor/rpi_wifi_config.sh
+++ solar_monitor/rpi_wifi_config.sh
@@ -7,13 +7,6 @@ if test -f "${WIFI_CONFIG_FILE}"; then
     echo "$WIFI_CONFIG_FILE found"
     DIR=`dirname ${WIFI_CONFIG_FILE}`
 
-    # check if already processed
-    DONE_FILE="${DIR}/PROCESSED.txt"
-    if test -f "${DONE_FILE}"; then
-        echo "${WIFI_CONFIG_FILE} already processed"
-        exit
-    fi
-
     . "$WIFI_CONFIG_FILE"
     rm -f /etc/NetworkManager/system-connections/*
     cat /home/pi/energyutils/solar_monitor/rpi.nmconnection.tmpl | \
@@ -22,9 +15,6 @@ if test -f "${WIFI_CONFIG_FILE}"; then
     chmod go-rw /etc/NetworkManager/system-connections/solarmon.nmconnection
     systemctl restart NetworkManager
     echo "Configured WiFi for ${SSID}"
-
-    # mark as processed
-    touch ${DONE_FILE}
 
     # reboot
     reboot
