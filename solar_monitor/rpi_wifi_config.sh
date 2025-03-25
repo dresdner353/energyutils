460a2b639db9ef0ca4c502e7f25a7623ead3f257
dresdner353 <dresdner353@gmail.com>
2025-03-25 21:29:30 +0000

    rework of wifi config to avoid rebooting

--- solar_monitor/rpi_wifi_config.sh
+++ solar_monitor/rpi_wifi_config.sh
@@ -3,23 +3,34 @@ set -e
 
 NM_CONN_DIR="/etc/NetworkManager/system-connections"
 NM_CONN_FILE="${NM_CONN_DIR}/solarmon.nmconnection"
+NM_CONN_TMPFILE="/tmp/solarmon.nmconnection"
 
 WIFI_CONFIG_FILE=`find /media -name wifi.txt | tail -1`
 
-if test -f "${WIFI_CONFIG_FILE}"; then
+if test -f "${WIFI_CONFIG_FILE}"
+then
     echo "$WIFI_CONFIG_FILE found"
-    DIR=`dirname ${WIFI_CONFIG_FILE}`
-
+    cat "$WIFI_CONFIG_FILE"
+    # source variables
     . "$WIFI_CONFIG_FILE"
-    rm -f ${NM_CONN_DIR}/*
-    cat /home/pi/energyutils/solar_monitor/rpi.nmconnection.tmpl | \
-        sed -e "s/__SSID__/${SSID}/g" -e "s/__PASSWORD__/${PASSWORD}/g" > ${NM_CONN_FILE}
-    chmod go-rw ${NM_CONN_FILE}
-    systemctl restart NetworkManager
-    echo "Configured WiFi for ${SSID}"
 
-    # reboot
-    /usr/sbin/reboot
+    # create NetworkManager connection file from template
+    cat /home/pi/energyutils/solar_monitor/rpi.nmconnection.tmpl | \
+        sed -e "s/__SSID__/${SSID}/g" -e "s/__PASSWORD__/${PASSWORD}/g" > ${NM_CONN_TMPFILE}
+
+    if cmp -s "${NM_CONN_TMPFILE}" "${NM_CONN_FILE}"
+    then
+        echo "No changes to NetworkManager configuration"
+    else
+        echo "Updating NetworkManager configuration"
+        # move to NetworkManager directory
+        # and restart NetworkManager
+        rm -f ${NM_CONN_DIR}/*
+        mv ${NM_CONN_TMPFILE} ${NM_CONN_FILE}
+        chmod go-rw ${NM_CONN_FILE}
+        systemctl restart NetworkManager
+        echo "Configured WiFi for ${SSID}"
+    fi
 fi
 
 
