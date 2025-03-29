3d9e8d75e1666facc331959ec054e40ae2d294af
dresdner353 <dresdner353@gmail.com>
2025-03-29 13:02:44 +0000

    small layout tweak on donut size

--- solar_monitor/www/dash.js
+++ solar_monitor/www/dash.js
@@ -435,7 +435,7 @@ function set_layout() {
     // 5:7 split columns
     // single donut on left, metrics stack on right
     small_layout = `
-                <div id="master" class="container-fluid" data-bs-theme="dark">
+                <div id="master" class="container-fluid align-middle" data-bs-theme="dark">
                     <div class="row align-items-center">
                         <div id="donut_a_insert" class="col col-5 mt-2 align-middle">
                         </div>
